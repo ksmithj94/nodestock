4a08c8527d282d87c88659e4c8878ee0d1b3d859
ksmithj94 <42583393+ksmithj94@users.noreply.github.com>
2020-12-10 13:00:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
-let express = require("express");
-let app = express();
-let path = require("path");
-let exphbs = require("express-handlebars");
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
-let PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
-//set Handlebars Middleware
-app.engine("handlebars", exphbs());
-app.set('view engine', "handlebars");
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
-//set handlebar routes
+
+// API KEY pk_062031d20883444f9ea74e2610fe2011
+// create call_api function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_062031d20883444f9ea74e2610fe2011', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
+
+// Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+
+// Set handlebar index GET route
 app.get('/', function (req, res) {
-  res.render('home');
+	call_api(function(doneAPI) {
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, "fb");
+		
 });
 
-// set static folder
-app.use(express.static(path.join(__dirname, "public")));
-
-
-app.listen(PORT, function() {
-  console.log('server listening on port ' + PORT)
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			//console.log(doneAPI);
+			//posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+		
 });
+
+
+
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
